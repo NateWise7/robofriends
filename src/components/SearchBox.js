9f6bd842ab0d3004ea0810caf6cd26985d60606d
NateWise7 <57462126+NateWise7@users.noreply.github.com>
2023-04-13 17:23:29 -0600

    adding source files

---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+	return (
+		<>
+			<input 
+			className='pa3 ba b--green bg-lightest-blue'
+			type='search' 
+			placeholder='Search Robots' 
+			onChange={ searchChange }
+			/>
+		</>
+	);
+}
+
+
+export default SearchBox;
